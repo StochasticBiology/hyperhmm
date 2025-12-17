77bc747a8c4f5c5502af27756e3fd2a9af38f9f0
Stochastic Biology Group <50171196+StochasticBiology@users.noreply.github.com>
2025-12-17 08:29:28 +0100

    Add files via upload

---
+++ R/dummy.R
@@ -0,0 +1,3 @@
+#' @useDynLib hyperhmm, .registration = TRUE
+#' @importFrom Rcpp evalCpp
+NULL
---
+++ R/hyperhmm.R
@@ -0,0 +1,13 @@
+#' Run HyperHMM on a dataset
+#'
+#' @param obs (matrix) a matrix of observations
+#' @param initialstates (matrix) optional initial states
+#' @param seed (integer, default 1) random number seed
+#' @param nboot (integer, default 100) number of bootstraps
+#' @param fullsample (integer) XXX
+#' @param outputinput (integer) XXX
+#'
+#' @return A fitted HyperHMM model
+#' @export
+#' @name HyperHMM
+NULL
